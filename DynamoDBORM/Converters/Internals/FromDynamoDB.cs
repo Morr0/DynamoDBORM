456ebaafdde3f3115acae8a1e761f86acd96c62c
Rami Hadad <rami@ramihikmat.net>
2020-11-29 16:16:53 +1100

    More types included

--- DynamoDBORM/Converters/Internals/FromDynamoDB.cs
+++ DynamoDBORM/Converters/Internals/FromDynamoDB.cs
@@ -8,7 +8,26 @@ namespace DynamoDBORM.Converters.Internals
     {
         internal static Dictionary<Type, Func<AttributeValue, object>> From = new Dictionary<Type, Func<AttributeValue, object>>
         {
+            // Strings
             { typeof(string), (val) => val.S},
+            { typeof(char), (val) => val.S.ToCharArray()[0]},
+            { typeof(char[]), (val) => val.S.ToCharArray()},
+            // Byte
+            { typeof(sbyte), (val) => sbyte.Parse(val.N)},
+            { typeof(byte), (val) => byte.Parse(val.N)},
+            // Signed numbers
+            { typeof(short), (val) => short.Parse(val.N)},
+            { typeof(int), (val) => int.Parse(val.N)},
+            { typeof(long), (val) => long.Parse(val.N)},
+            { typeof(float), (val) => float.Parse(val.N)},
+            { typeof(double), (val) => double.Parse(val.N)},
+            { typeof(decimal), (val) => decimal.Parse(val.N)},
+            // Unsigned integers
+            { typeof(ushort), (val) => ushort.Parse(val.N)},
+            { typeof(uint), (val) => uint.Parse(val.N)},
+            { typeof(ulong), (val) => ulong.Parse(val.N)},
+            // Bool
+            { typeof(bool), (val) => val.BOOL}
         };
     }
 }
--- DynamoDBORM/Converters/Internals/ToDynamoDB.cs
+++ DynamoDBORM/Converters/Internals/ToDynamoDB.cs
@@ -8,7 +8,26 @@ namespace DynamoDBORM.Converters.Internals
     {
         internal static Dictionary<Type, Func<object, AttributeValue>> To = new Dictionary<Type, Func<object, AttributeValue>>
         {
+            // Strings
+            { typeof(char), (obj) => new AttributeValue{ S = obj?.ToString()}},
             { typeof(string), (obj) => new AttributeValue{ S = obj?.ToString()}},
+            { typeof(char[]), (obj) => new AttributeValue{ S = new string((char[])obj)}},
+            // Byte
+            { typeof(byte), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            { typeof(sbyte), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            // Signed numbers
+            { typeof(short), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            { typeof(int), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            { typeof(long), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            { typeof(float), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            { typeof(double), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            { typeof(decimal), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            // Unsigned integers
+            { typeof(ushort), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            { typeof(uint), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            { typeof(ulong), (obj) => new AttributeValue{ N = obj?.ToString()}},
+            // Bool
+            { typeof(bool), (obj) => new AttributeValue{ BOOL = (bool)obj}}
         };
     }
 }
